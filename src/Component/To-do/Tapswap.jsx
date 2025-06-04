621b13cfd8d035496b2e056e375ed03c63ff0a5e
Emmanuel Ntiti <ntitiemmanuel@gmail.com>
2025-06-04 15:02:49 +0100

    Changes to be committed

--- src/Component/To-do/Tapswap.jsx
+++ src/Component/To-do/Tapswap.jsx
@@ -12,8 +12,8 @@ function TapSwap() {
         <div style={{ padding: '20px', maxWidth: '400px', margin: 'auto' }}>
             <h1>Tap Swap Component</h1>
             <p>Count: {count}</p>
-           <button onClick={Plus}>+1</button>
-           <button onClick={Minus}>-1</button>
+           <button onClick={Plus} style={{padding: '20px',marginRight:'10px', background: 'green', borderRadius: '30px'}}>+1</button>
+           <button onClick={Minus}style={{padding: '20px',marginRight:'10px', background: 'red', borderRadius: '30px'}}>-1</button>
            <button onClick={() => setCount(0)}>Reset</button>
         </div>
     );
